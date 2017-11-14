3b64b05985e54ed31122234870eba141d4f2034b
jbhouse <jbmoorhouse1@gmail.com>
2017-11-14 15:39:26 -0500

    created email template filling app using regex

---
+++ src/EmailCreatorApp.java
@@ -0,0 +1,43 @@
+
+public class EmailCreatorApp {
+
+	public static void main(String[] args) {
+		System.out.println("Email Creator\n");
+		System.out.println("=====================================================");
+		String[] csv = {"   james   ,butler,jbutler@gmail.com",
+                "Josephine,Darakjy,Josephine_Darakjy@darakjy.org",
+                "ART,VENERE,ART@VENERE.ORG"};
+
+		String template = 
+			    "To:      {email}\n" +
+			    "From:    noreply@deals.com\n" +
+			    "Subject: Deals!\n\n" +
+			    "Hi {first_name},\n\n" +
+			    "We've got some great deals for you. Check our website!";
+
+		for (int i = 0; i < csv.length; i++) {
+			insertInfoIntoTemplate(csv[i], template);
+		}
+	}
+	
+	public static void insertInfoIntoTemplate(String contactInfo, String template) {
+		StringBuilder sb = new StringBuilder(template);
+		String[] formattedInfo = contactInfo.replaceAll("\\s","").toLowerCase().split(",");
+		for (int i = 0; i < formattedInfo.length; i++) {
+			if (i<2) {
+				formattedInfo[i] = formattedInfo[i].substring(0, 1).toUpperCase() + formattedInfo[i].substring(1);
+			}
+			if (i==0) {
+				template = template.replaceFirst("first_name",formattedInfo[i]);
+			}
+			if (i==2) {
+				template = template.replaceFirst("email",formattedInfo[i]);
+			}
+		}
+		template = template.replaceAll("\\}", "");
+		System.out.println(template.replaceAll("\\{", ""));
+		System.out.println("=====================================================");
+		System.out.println();
+	}
+
+}
